773bdec9ef4f26a112c05982940e5a46cdd64fd2
Zhen- Gao-Git-Kraken <bioinfo.gao@gmail.com>
2019-06-26 18:43:56 -0400

    commit python 6 again

--- pythonEmbededInR6.py
+++ pythonEmbededInR6.py
@@ -1,3 +1,4 @@
 print("hello from python embeded in Rstudio")
 print("hello from python embeded in Rstudio")
+print("hello from python embeded in Rstudiozzzzz")
 print("hello from python embeded in Rstudioxxxxxxyyyyy")
